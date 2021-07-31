6464d823aaf7c54d209ec82189f66eb4c64f80de
weakennN <lubomedi03@gmail.com>
2021-07-31 23:35:44 +0300

    create the nav bar

---
+++ smt.php
@@ -0,0 +1,9 @@
+<?php
+function doSmth()
+{
+    echo "<p id='id'>TEST</p>";
+}
+
+doSmth();
+// $_COOKIE ["biskvita"] = 10;
+// setcookie("smt", 10, time() + 20, '/');
---
+++ test.js
@@ -0,0 +1,4 @@
+function test() {
+    console.log( document.getElementById("id").innerHTML);
+    document.getElementById("button").innerHTML = "Hello World";
+}
